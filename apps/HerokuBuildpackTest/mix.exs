41da3ef61cab48d8644fa90903bf46fd7b74bd9a
DanMHammer <35697323+DanMHammer@users.noreply.github.com>
2018-09-06 15:42:35 -0500

    Update mix.exs

--- apps/HerokuBuildpackTest/mix.exs
+++ apps/HerokuBuildpackTest/mix.exs
@@ -3,16 +3,16 @@ defmodule HerokuBuildpackTest.Mixfile do
 
   def project do
     [
-    app: :HerokuBuildpackTest,
-     version: "0.0.1",
-	 build_path: "../../_build",
-	 config_path: "../../config/config.exs",
-	 deps_path: "../../deps",
-	 lockfile: "../../mix.lock",
-     elixir: "~> 1.1",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()
+      app: :HerokuBuildpackTest,
+      version: "0.0.1",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
+      elixir: "~> 1.1",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps()
      ]
   end
 
