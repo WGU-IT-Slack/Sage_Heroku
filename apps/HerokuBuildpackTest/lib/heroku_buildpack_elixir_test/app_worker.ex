8c1bce56922805726343b76cd4cc7fba68d3e4af
DanMHammer <35697323+DanMHammer@users.noreply.github.com>
2019-07-20 12:32:34 -0500

    Update app_worker.ex

--- apps/HerokuBuildpackTest/lib/heroku_buildpack_elixir_test/app_worker.ex
+++ apps/HerokuBuildpackTest/lib/heroku_buildpack_elixir_test/app_worker.ex
@@ -1,7 +1,6 @@
 defmodule HerokuBuildpackTest.AppWorker do
 
   def start_link do
-    Plug.Adapters.Cowboy.http MyPlug, [], port: 8080
-  end
-
+    port = System.get_env("PORT") |> String.to_integer
+    Plug.Adapters.Cowboy.http MyPlug, [], port: port  end
 end
